2d082a580c9337134e6cad4d50fc518b8b2191e4
Vivek Kothari <5404922+vivekkothari@users.noreply.github.com>
2025-02-09 22:06:20 +0530

    exception handler

--- src/main/kotlin/com/github/vivekkothari/scheduler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/github/vivekkothari/scheduler/GlobalExceptionHandler.kt
@@ -3,8 +3,11 @@ package com.github.vivekkothari.scheduler
 import com.github.kagkarlsson.scheduler.exceptions.TaskInstanceNotFoundException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.FieldError
+import org.springframework.validation.ObjectError
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.support.WebExchangeBindException
 
 /** Global exception handler. */
 @ControllerAdvice
@@ -14,5 +17,17 @@ class GlobalExceptionHandler {
   @ExceptionHandler(TaskInstanceNotFoundException::class)
   fun handleTaskNotFoundException(): ResponseEntity<String> {
     return ResponseEntity("Task not found", HttpStatus.NOT_FOUND)
+  }
+
+  /** Handle MethodArgumentNotValidException. */
+  @ExceptionHandler(WebExchangeBindException::class)
+  fun handleValidationExceptions(ex: WebExchangeBindException): ResponseEntity<Any> {
+    val errors: MutableMap<String, String?> = HashMap()
+    ex.bindingResult.allErrors.forEach { error: ObjectError ->
+      val fieldName = (error as FieldError).field
+      val errorMessage = error.getDefaultMessage()
+      errors[fieldName] = errorMessage
+    }
+    return ResponseEntity(errors, HttpStatus.BAD_REQUEST)
   }
 }
