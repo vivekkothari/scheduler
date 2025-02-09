990215721678dddd065e2b6d9f6e1a7d9afb1bd4
Vivek Kothari <5404922+vivekkothari@users.noreply.github.com>
2025-02-09 19:48:40 +0530

    add names to operations

--- src/main/kotlin/com/github/vivekkothari/scheduler/SchedulerController.kt
+++ src/main/kotlin/com/github/vivekkothari/scheduler/SchedulerController.kt
@@ -2,21 +2,26 @@ package com.github.vivekkothari.scheduler
 
 import com.github.vivekkothari.scheduler.dto.ScheduleTaskRequest
 import com.github.vivekkothari.scheduler.dto.ScheduleTaskResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
 
 /** The controller which exposes the REST apis to interact with the app. */
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api", consumes = ["application/json"], produces = ["application/json"])
+@Tag(name = "Scheduler", description = "Scheduler API")
 data class SchedulerController(private val schedulerService: SchedulerService) {
 
   /** Schedule a task. */
   @PutMapping("/schedule")
+  @Operation(summary = "Schedule a task")
   fun scheduleTask(@RequestBody task: ScheduleTaskRequest): ScheduleTaskResponse {
     return schedulerService.scheduleTask(task)
   }
 
   /** Cancel a task. */
   @DeleteMapping("/schedule/{id}")
+  @Operation(summary = "Cancel a task")
   fun cancelTask(@PathVariable("id") id: String) {
     schedulerService.cancelTask(id)
   }
